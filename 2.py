84478b3ff45e2abffcff16b9539befb5627928c6
kumawatridam <kumawatridam@gmail.com>
2026-07-07 16:34:56 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+age=int (input("enter the the age"))
+citizen=input("are u citizen of india:True/False?")
+if age>=18 and citizen=="True" or citizen=="true":
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
