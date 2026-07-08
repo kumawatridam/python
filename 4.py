5e5738cd97b2faff206841acdd6c1acd288ca8c5
kumawatridam <kumawatridam@gmail.com>
2026-07-08 11:52:58 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the number 1"))
+num2=int(input("ente4 the number 2"))
+#summ of number
+print("sum:",num1+num2)
+print("diffrence",num1-num2)
+print("multiplication",num1*num2)
+print("division",num1/num2)
